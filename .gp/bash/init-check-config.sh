6cff20226f3759bfbad669fd4376f329c75ca132
JohntheFish <john@c5magic.co.uk>
2023-12-06 16:42:52 +0000

    Update Info.php
    Provide database entities status

--- .gp/bash/init-check-config.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-#
-# SPDX-License-Identifier: MIT
-# Copyright © 2021 Apolo Pena
-#
-# init-check-config.sh
-# Description:
-# Detects and logs any possible mismatched configuration values.
