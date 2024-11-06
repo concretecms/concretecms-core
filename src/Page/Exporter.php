ca641123c693d74f2e522953fb51ba25371852e8
Michele Locati <michele@locati.it>
2024-11-06 14:09:06 +0100

    Export locale home pages and multilingual mappings

--- src/Page/Exporter.php
+++ src/Page/Exporter.php
@@ -2,37 +2,53 @@
 namespace Concrete\Core\Page;
 
 use Concrete\Core\Area\Area;
+use Concrete\Core\Database\Connection\Connection;
+use Concrete\Core\Entity\Site\Locale;
 use Concrete\Core\Export\Item\ItemInterface;
-use Concrete\Core\Support\Facade\Facade;
-use Concrete\Core\User\UserInfo;
+use Concrete\Core\Multilingual\Page\Section\Section;
+use Concrete\Core\User\UserInfoRepository;
+use Doctrine\ORM\EntityManagerInterface;
+use SimpleXMLElement;
 
 class Exporter implements ItemInterface
 {
-
     /**
-     * @param Page $mixed
-     * @param \SimpleXMLElement $element
+     * @param \Concrete\Core\Page\Page $mixed
      */
-    public function export($mixed, \SimpleXMLElement $element)
+    public function export($mixed, SimpleXMLElement $element)
     {
-        $app = Facade::getFacadeApplication();
-        $pageNode = $element;
-        $p = $pageNode->addChild('page');
-        $p->addAttribute('name', $app->make('helper/text')->entities($mixed->getCollectionName()));
+        $p = $element->addChild('page');
+        $p->addAttribute('name', $mixed->getCollectionName());
         $p->addAttribute('path', $mixed->getCollectionPath());
         $p->addAttribute('public-date', $mixed->getCollectionDatePublic());
         $p->addAttribute('filename', $mixed->getCollectionFilename());
         $p->addAttribute('pagetype', $mixed->getPageTypeHandle());
-        $template = Template::getByID($mixed->getPageTemplateID());
-        if (is_object($template)) {
-            $p->addAttribute('template', $template->getPageTemplateHandle());
+        $locale = $this->getLocaleForHome($mixed);
+        if ($locale !== null) {
+            $this->exportLocaleRoot($p, $locale);
         }
-        $ui = UserInfo::getByID($mixed->getCollectionUserID());
-        if (!is_object($ui)) {
-            $ui = UserInfo::getByID(USER_SUPER_ID);
+        $hrefLangMap = $this->getHrefLangMap($mixed);
+        if ($hrefLangMap !== []) {
+            $this->exportHrefLangMap($p, $hrefLangMap);
+        }
+        $templateID = $mixed->getPageTemplateID();
+        if ($templateID) {
+            $template = app(EntityManagerInterface::class)->find(\Concrete\Core\Entity\Page\Template::class, $templateID);
+            if ($template) {
+                $p->addAttribute('template', $template->getPageTemplateHandle());
+            }
+        }
+        $uiRepository = app(UserInfoRepository::class);
+        $ui = null;
+        $uID = $mixed->getCollectionUserID();
+        if ($uID) {
+            $ui = $uiRepository->getByID($uID);
+        }
+        if ($ui === null) {
+            $ui = $uiRepository->getByID(USER_SUPER_ID);
         }
         $p->addAttribute('user', $ui->getUserName());
-        $p->addAttribute('description', $app->make('helper/text')->entities($mixed->getCollectionDescription()));
+        $p->addAttribute('description', $mixed->getCollectionDescription());
         $p->addAttribute('package', $mixed->getPackageHandle());
         if ($mixed->getCollectionParentID() == 0) {
             if ($mixed->getSiteTreeID() == 0) {
@@ -43,7 +59,7 @@ class Exporter implements ItemInterface
         }
 
         $attribs = $mixed->getSetCollectionAttributes();
-        if (count($attribs) > 0) {
+        if ($attribs !== []) {
             $attributes = $p->addChild('attributes');
             foreach ($attribs as $ak) {
                 $av = $mixed->getAttributeValueObject($ak);
@@ -55,12 +71,83 @@ class Exporter implements ItemInterface
             }
         }
 
-        $db = \Database::connection();
-        $r = $db->executeQuery('select arHandle from Areas where cID = ? and arIsGlobal = 0 and arParentID = 0', [$mixed->getCollectionID()]);
+        $r = app(Connection::class)->executeQuery('select arHandle from Areas where cID = ? and arIsGlobal = 0 and arParentID = 0', [$mixed->getCollectionID()]);
         while ($row = $r->FetchRow()) {
             $ax = Area::get($mixed, $row['arHandle']);
             $ax->export($p, $mixed);
         }
     }
 
+    /**
+     * @return \Concrete\Core\Entity\Site\Locale|null
+     */
+    private function getLocaleForHome(Page $page)
+    {
+        $siteTreeID = $page->getSiteTreeID();
+        if (!$siteTreeID) {
+            return null;
+        }
+        $section = Section::getByID($page->getCollectionID());
+
+        return $section ? $section->getLocaleObject() : null;
+    }
+
+    private function exportLocaleRoot(SimpleXMLElement $parentElement, Locale $locale)
+    {
+        $localeElement = $parentElement->addChild('locale');
+        $localeElement->addAttribute('language', $locale->getLanguage());
+        $country = (string) $locale->getCountry();
+        if ($country !== '') {
+            $localeElement->addAttribute('country', $country);
+        }
+    }
+
+    private function getHrefLangMap(Page $page)
+    {
+        $pageSection = Section::getBySectionOfSite($page);
+        if (!$pageSection) {
+            return [];
+        }
+        $site = $pageSection->getSite();
+        if (!$site) {
+            return [];
+        }
+        $map = [];
+        foreach (Section::getList($site) as $relatedSection) {
+            $relatedLocale = $relatedSection->getLocale();
+            if ($pageSection->getLocale() === $relatedLocale) {
+                continue;
+            }
+            $relatedPageID = $relatedSection->getTranslatedPageID($page);
+            if (!$relatedPageID) {
+                continue;
+            }
+            $relatedPage = Page::getByID($relatedPageID);
+            if (!$relatedPage || $relatedPage->isError()) {
+                continue;
+            }
+            $relatedPagePath = (string) $relatedPage->getCollectionPath();
+            if ($relatedPagePath === '' && Section::isMultilingualSection($relatedPageID)) {
+                $relatedPagePath = (string) $relatedSection->getCollectionPath();
+                if ($relatedPagePath === '') {
+                    $relatedPagePath = '/';
+                }
+            }
+            if ($relatedPagePath === '') {
+                continue;
+            }
+            $map[$relatedLocale] = $relatedPagePath;
+        }
+        return $map;
+    }
+
+    private function exportHrefLangMap(SimpleXMLElement $parentElement, array $map)
+    {
+        $hrefLangElement = $parentElement->addChild('hreflang');
+        foreach ($map as $locale => $path) {
+            $alternateElement = $hrefLangElement->addChild('alternate');
+            $alternateElement->addAttribute('locale', $locale);
+            $alternateElement->addAttribute('path', $path);
+        }
+    }
 }
