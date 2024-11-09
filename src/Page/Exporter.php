029671eb40e06d0b643091a4045e89bf38c11c76
Michele Locati <michele@locati.it>
2024-11-09 11:47:55 +0100

    Add support for exporting/importing page aliases

--- src/Page/Exporter.php
+++ src/Page/Exporter.php
@@ -12,15 +12,33 @@ use SimpleXMLElement;
 
 class Exporter implements ItemInterface
 {
+    const TYPE_PAGE = 'page';
+    const TYPE_ALIAS = 'alias';
+    const TYPE_EXTERNALLINK = 'external-link';
+
     /**
      * @param \Concrete\Core\Page\Page $mixed
      */
     public function export($mixed, SimpleXMLElement $element)
     {
-        $isExternalLink = $mixed->isExternalLink();
-        $p = $element->addChild($isExternalLink ? 'external-link' : 'page');
+        if ($mixed->isExternalLink()) {
+            $type = self::TYPE_EXTERNALLINK;
+        } elseif ($mixed->isAliasPage()) {
+            $type = self::TYPE_ALIAS;
+            $aliasedPage = Page::getByID($mixed->getCollectionPointerID());
+        } else {
+            $type = self::TYPE_PAGE;
+        }
+        $p = $element->addChild($type);
         $p->addAttribute('name', $mixed->getCollectionName());
-        $p->addAttribute('path', $isExternalLink ? $mixed->generatePagePath() : $mixed->getCollectionPath());
+        switch ($type) {
+            case self::TYPE_EXTERNALLINK:
+                $p->addAttribute('path', $mixed->generatePagePath());
+                break;
+            default:
+                $p->addAttribute('path', $mixed->getCollectionPath());
+                break;
+        }
         $uiRepository = app(UserInfoRepository::class);
         $ui = null;
         $uID = $mixed->getCollectionUserID();
@@ -30,56 +48,65 @@ class Exporter implements ItemInterface
         if ($ui === null) {
             $ui = $uiRepository->getByID(USER_SUPER_ID);
         }
-        $p->addAttribute('user', $ui->getUserName());
-        $p->addAttribute('public-date', $mixed->getCollectionDatePublic());
-        if ($isExternalLink) {
-            $p->addAttribute('destination', $mixed->getCollectionPointerExternalLink());
-            $p->addAttribute('new-window', $mixed->openCollectionPointerExternalLinkInNewWindow() ? 'true' : 'false');
-        } else {
-            $p->addAttribute('filename', $mixed->getCollectionFilename());
-            $p->addAttribute('pagetype', $mixed->getPageTypeHandle());
-            $locale = $this->getLocaleForHome($mixed);
-            if ($locale !== null) {
-                $this->exportLocaleRoot($p, $locale);
-            }
-            $this->exportAdditionalPagePaths($p, $mixed);
-            $hrefLangMap = $this->getHrefLangMap($mixed);
-            if ($hrefLangMap !== []) {
-                $this->exportHrefLangMap($p, $hrefLangMap);
-            }
-            $templateID = $mixed->getPageTemplateID();
-            if ($templateID) {
-                $template = app(EntityManagerInterface::class)->find(\Concrete\Core\Entity\Page\Template::class, $templateID);
-                if ($template) {
-                    $p->addAttribute('template', $template->getPageTemplateHandle());
+        switch ($type) {
+            case self::TYPE_EXTERNALLINK:
+                $p->addAttribute('destination', $mixed->getCollectionPointerExternalLink());
+                $p->addAttribute('new-window', $mixed->openCollectionPointerExternalLinkInNewWindow() ? 'true' : 'false');
+                $p->addAttribute('user', $ui->getUserName());
+                $p->addAttribute('public-date', $mixed->getCollectionDatePublic());
+                break;
+            case self::TYPE_ALIAS:
+                $p->addAttribute('original-path', $aliasedPage->getCollectionPath());
+                $p->addAttribute('user', $ui->getUserName());
+                break;
+            case self::TYPE_PAGE:
+                $p->addAttribute('user', $ui->getUserName());
+                $p->addAttribute('public-date', $mixed->getCollectionDatePublic());
+                $p->addAttribute('filename', $mixed->getCollectionFilename());
+                $p->addAttribute('pagetype', $mixed->getPageTypeHandle());
+                $locale = $this->getLocaleForHome($mixed);
+                if ($locale !== null) {
+                    $this->exportLocaleRoot($p, $locale);
                 }
-            }
-            $p->addAttribute('description', $mixed->getCollectionDescription());
-            if ($mixed->getCollectionParentID() == 0) {
-                if ($mixed->getSiteTreeID() == 0) {
-                    $p->addAttribute('global', 'true');
-                } else {
-                    $p->addAttribute('root', 'true');
+                $this->exportAdditionalPagePaths($p, $mixed);
+                $hrefLangMap = $this->getHrefLangMap($mixed);
+                if ($hrefLangMap !== []) {
+                    $this->exportHrefLangMap($p, $hrefLangMap);
                 }
-            }
-            $attribs = $mixed->getSetCollectionAttributes();
-            if ($attribs !== []) {
-                $attributes = $p->addChild('attributes');
-                foreach ($attribs as $ak) {
-                    $av = $mixed->getAttributeValueObject($ak);
-                    $cnt = $ak->getController();
-                    $cnt->setAttributeValue($av);
-                    $akx = $attributes->addChild('attributekey');
-                    $akx->addAttribute('handle', $ak->getAttributeKeyHandle());
-                    $cnt->exportValue($akx);
+                $templateID = $mixed->getPageTemplateID();
+                if ($templateID) {
+                    $template = app(EntityManagerInterface::class)->find(\Concrete\Core\Entity\Page\Template::class, $templateID);
+                    if ($template) {
+                        $p->addAttribute('template', $template->getPageTemplateHandle());
+                    }
                 }
-            }
-    
-            $r = app(Connection::class)->executeQuery('select arHandle from Areas where cID = ? and arIsGlobal = 0 and arParentID = 0', [$mixed->getCollectionID()]);
-            while ($row = $r->FetchRow()) {
-                $ax = Area::get($mixed, $row['arHandle']);
-                $ax->export($p, $mixed);
-            }
+                $p->addAttribute('description', $mixed->getCollectionDescription());
+                if ($mixed->getCollectionParentID() == 0) {
+                    if ($mixed->getSiteTreeID() == 0) {
+                        $p->addAttribute('global', 'true');
+                    } else {
+                        $p->addAttribute('root', 'true');
+                    }
+                }
+                $attribs = $mixed->getSetCollectionAttributes();
+                if ($attribs !== []) {
+                    $attributes = $p->addChild('attributes');
+                    foreach ($attribs as $ak) {
+                        $av = $mixed->getAttributeValueObject($ak);
+                        $cnt = $ak->getController();
+                        $cnt->setAttributeValue($av);
+                        $akx = $attributes->addChild('attributekey');
+                        $akx->addAttribute('handle', $ak->getAttributeKeyHandle());
+                        $cnt->exportValue($akx);
+                    }
+                }
+        
+                $r = app(Connection::class)->executeQuery('select arHandle from Areas where cID = ? and arIsGlobal = 0 and arParentID = 0', [$mixed->getCollectionID()]);
+                while ($row = $r->FetchRow()) {
+                    $ax = Area::get($mixed, $row['arHandle']);
+                    $ax->export($p, $mixed);
+                }
+                break;
         }
         $p->addAttribute('package', $mixed->getPackageHandle());
     }
