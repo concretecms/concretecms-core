f06860b15e0b341377e5c2d392caca7b7186e841
Michele Locati <michele@locati.it>
2024-11-14 17:43:04 +0100

    Add "storage" attribute to config/option elements

--- src/Backup/ContentImporter/Importer/Routine/ImportConfigValuesRoutine.php
+++ src/Backup/ContentImporter/Importer/Routine/ImportConfigValuesRoutine.php
@@ -1,42 +1,78 @@
 <?php
+
 namespace Concrete\Core\Backup\ContentImporter\Importer\Routine;
 
-use Concrete\Core\Attribute\Type;
-use Concrete\Core\Block\BlockType\BlockType;
-use Concrete\Core\Permission\Category;
-use Concrete\Core\Validation\BannedWord\BannedWord;
+use Concrete\Core\Entity\Package;
+use SimpleXMLElement;
 
 class ImportConfigValuesRoutine extends AbstractRoutine
 {
+    private $repositoryInstances;
+
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Backup\ContentImporter\Importer\Routine\RoutineInterface::getHandle()
+     */
     public function getHandle()
     {
         return 'config_values';
     }
 
-    public function import(\SimpleXMLElement $sx)
+    /**
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Backup\ContentImporter\Importer\Routine\RoutineInterface::import()
+     */
+    public function import(SimpleXMLElement $sx)
     {
-        if (isset($sx->config)) {
-            foreach ($sx->config->children() as $key) {
-                $pkg = static::getPackageObject($key['package']);
-                $node = (string) $key->getName();
-                if ($node != 'option') {
-                    // legacy
-                    $option = $node;
-                } else {
-                    $option = (string) $key['name'];
-                }
-
-                $value = (string) $key;
-                if ($value === 'false') {
-                    $value = false;
-                }
-                if (is_object($pkg)) {
-                    \Config::save($pkg->getPackageHandle() . '::' . $option, $value);
-                } else {
-                    \Config::save($option, $value);
-                }
+        if (!isset($sx->config)) {
+            return;
+        }
+        $this->repositoryInstances = [];
+        foreach ($sx->config->children() as $key) {
+            $package = isset($key['package']) ? static::getPackageObject($key['package']) : null;
+            $node = (string) $key->getName();
+            if ($node !== 'option') {
+                // legacy
+                $key = $node;
+            } else {
+                $key = (string) $key['name'];
             }
+            $value = (string) $key;
+            if ($value === 'false') {
+                $value = false;
+            }
+            $repository = $this->getRepository(isset($key['storage']) ? (string) $key['storage'] : '', $package);
+            $repository->set($key, $value);
+            $repository->save($key, $value);
         }
     }
 
+    /**
+     * @return \Concrete\Core\Config\Repository\Repository|\Concrete\Core\Config\Repository\Liaison
+     */
+    private function getRepository(string $storage, ?Package $package)
+    {
+        switch ($storage) {
+            case 'database':
+                break;
+            default:
+                $storage = 'file';
+        }
+        $key = $storage . ($package ? "@{$package->getPackageHandle()}" : '');
+        if (isset($this->repositoryInstances[$key])) {
+            return $this->repositoryInstances[$key];
+        }
+        switch ($storage) {
+            case 'database':
+                $repository = $package ? $package->getController()->getDatabaseConfig() : app('config/database');
+                break;
+            case 'file':
+                $repository = $package ? $package->getController()->getFileConfig() : app('config');
+        }
+        $this->repositoryInstances[$key] = $repository;
+
+        return $repository;
+    }
 }
