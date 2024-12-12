bc812583fdc64a88e71009daa90cafdd76f6ba81
Michele Locati <michele@locati.it>
2024-12-12 14:48:22 +0100

    Use DI to create the ErrorHandler instance

--- src/Error/Provider/ErrorHandlingServiceProvider.php
+++ src/Error/Provider/ErrorHandlingServiceProvider.php
@@ -5,15 +5,13 @@ use Concrete\Core\Foundation\Service\Provider;
 use Concrete\Core\Error\Handling\ErrorHandler;
 use Concrete\Core\Logging\Channels;
 use Concrete\Core\Logging\LoggerFactory;
-use Symfony\Component\ErrorHandler\ErrorHandler as SymfonyErrorHandler;
 
 class ErrorHandlingServiceProvider extends Provider
 {
     public function register()
     {
         $logger = $this->app->make(LoggerFactory::class)->createLogger(Channels::CHANNEL_EXCEPTIONS);
-        $config = $this->app->make('config');
-        $handler = new ErrorHandler($logger, $config);
+        $handler = $this->app->make(ErrorHandler::class, ['logger' => $logger]);
         $handler = ErrorHandler::register($handler);
         $handler->setExceptionHandler([$handler, 'renderConcreteException']);
     }
