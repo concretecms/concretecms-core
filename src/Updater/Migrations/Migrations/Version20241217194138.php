d153cdbb85562b397f29122d59f191b892752bd7
Andrew Embler <andrew@concrete5.org>
2024-12-29 09:36:09 -0800

    Adding summary templates dashboard page

--- src/Updater/Migrations/Migrations/Version20241217194138.php
+++ src/Updater/Migrations/Migrations/Version20241217194138.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Entity\Board\Instance;
+use Concrete\Core\Page\Page;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
@@ -13,5 +14,10 @@ final class Version20241217194138 extends AbstractMigration implements Repeatabl
     public function upgradeDatabase()
     {
         $this->refreshEntities([Instance::class]);
+        $c = Page::getByPath('/dashboard/system/boards');
+        if (is_object($c) && !$c->isError()) {
+            $c->update(['cName' => 'Boards &amp; Summary']);
+        }
+        $this->createSinglePage('/dashboard/system/boards/summary_templates', 'Summary Templates');
     }
 }
