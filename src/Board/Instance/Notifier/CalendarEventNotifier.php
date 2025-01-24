24271b8e0605f5ad1479e288085b7f5b5549d7e2
Michele Locati <michele@locati.it>
2025-01-24 10:58:48 +0100

    Add PHPDoc

--- src/Board/Instance/Notifier/CalendarEventNotifier.php
+++ src/Board/Instance/Notifier/CalendarEventNotifier.php
@@ -3,23 +3,24 @@
 namespace Concrete\Core\Board\Instance\Notifier;
 
 use Concrete\Core\Entity\Board\DataSource\Configuration\CalendarEventConfiguration;
-use Concrete\Core\Entity\Calendar\CalendarEvent;
 
 defined('C5_EXECUTE') or die("Access Denied.");
 
 class CalendarEventNotifier extends AbstractNotifier
 {
-
     /**
-     * @param CalendarEvent $object
-     * @return array
+     * {@inheritdoc}
+     *
+     * @see \Concrete\Core\Board\Instance\Notifier\NotifierInterface::findBoardInstancesThatMayContainObject()
+     *
+     * @param \Concrete\Core\Entity\Calendar\CalendarEvent $object
      */
     public function findBoardInstancesThatMayContainObject($object): array
     {
         $site = $object->getCalendar()->getSite();
         $instances = $this->filterBySite($site);
         $instances = $this->filterByHasConfiguration($instances, CalendarEventConfiguration::class);
+
         return $instances;
     }
 }
-
