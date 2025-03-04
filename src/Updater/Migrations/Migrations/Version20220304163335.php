cbe31ebd42927e5f5f17b62516628fce111ac7aa
Andrew Embler <andrew@concrete5.org>
2025-03-04 10:22:16 -0800

    Fixing upgrade (1)

--- src/Updater/Migrations/Migrations/Version20220304163335.php
+++ src/Updater/Migrations/Migrations/Version20220304163335.php
@@ -5,6 +5,7 @@ declare(strict_types=1);
 namespace Concrete\Core\Updater\Migrations\Migrations;
 
 use Concrete\Core\Application\UserInterface\Dashboard\Navigation\NavigationCache;
+use Concrete\Core\Entity\Site\Site;
 use Concrete\Core\Updater\Migrations\AbstractMigration;
 use Concrete\Core\Updater\Migrations\RepeatableMigrationInterface;
 
@@ -15,6 +16,7 @@ final class Version20220304163335 extends AbstractMigration implements Repeatabl
      */
     public function upgradeDatabase()
     {
+        $this->refreshEntities([Site::class]);
         $this->createSinglePage('/dashboard/system/environment/security', 'Security Policy', [
             'meta_keywords' => 'security, content security policy, csp, strict transport security, hsts, x-frame-options'
         ]);
